161212d65613ee6862531db128fa0148d8e424eb
daya98 <daya.aeist@gmail.com>
2020-11-02 01:09:47 +0700

    add hilt anotation in view

--- app/src/main/java/com/daya/taha/MainActivity.kt
+++ app/src/main/java/com/daya/taha/MainActivity.kt
@@ -2,10 +2,13 @@ package com.daya.taha
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.daya.taha.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val binding :  ActivityMainBinding by viewBinding(R.id.container)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/daya/taha/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/daya/taha/ui/dashboard/DashboardFragment.kt
@@ -1,27 +1,12 @@
 package com.daya.taha.ui.dashboard
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.daya.taha.R
+import com.daya.taha.databinding.FragmentDashboardBinding
 import dagger.hilt.android.AndroidEntryPoint
-
-class DashboardFragment : Fragment() {
-
-    //private val dashboardViewModel  by viewModels<DashboardViewModel>()
-
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-
-        return root
-    }
+@AndroidEntryPoint
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+    private val binding : FragmentDashboardBinding by viewBinding()
 }
