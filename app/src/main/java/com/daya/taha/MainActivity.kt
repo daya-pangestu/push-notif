4cb8f1fd2c9f5d9126dbc5e9e5d39415a5607dfb
ad <da>
2021-08-05 19:48:26 +0700

    navigation from splash to home

--- app/src/main/java/com/daya/taha/MainActivity.kt
+++ app/src/main/java/com/daya/taha/MainActivity.kt
@@ -2,6 +2,7 @@ package com.daya.taha
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.daya.taha.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+
     }
 }
 
