ac35770319815b66689a822ec5e94ede9c6cb0a3
ad <da>
2021-08-05 20:43:38 +0700

    re-added dummy test due jacocoRootReport keep failing without it (lazy fix)

---
+++ app/src/androidTest/java/com/daya/taha/ExampleInstrumetedTest.kt
@@ -0,0 +1,19 @@
+package com.daya.taha
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+//need dumy test for jacoco not fail
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.daya.taha", appContext.packageName)
+    }
+}
