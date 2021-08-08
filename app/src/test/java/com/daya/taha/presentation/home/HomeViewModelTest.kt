03b814c611b8a7a31863f21815d5e651b3fa18c1
ad <da>
2021-08-08 13:49:10 +0700

    add unit test for HomeViewModel

---
+++ app/src/test/java/com/daya/taha/presentation/home/HomeViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.daya.taha.presentation.home
+
+
+import com.daya.shared.taha.data.Resource
+import com.daya.shared.taha.domain.usecase.LogOutUseCase
+import com.daya.taha.utils.MainCoroutineRule
+import com.daya.taha.utils.runBlockingTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class HomeViewModelTest {
+
+    @get:Rule
+    var coroutineRule = MainCoroutineRule()
+
+    private val logOutUseCase: LogOutUseCase = mock()
+    lateinit var homeViewModel: HomeViewModel
+
+    private val dummyResSucces = Resource.success(Unit)
+
+    @Before
+    fun setUp() {
+        homeViewModel = HomeViewModel(logOutUseCase)
+    }
+
+    @Test
+    fun logOut() = coroutineRule.runBlockingTest {
+        whenever(logOutUseCase(Unit)).thenReturn(dummyResSucces)
+        val result = homeViewModel.logOut()
+        assertThat(result).isEqualTo(dummyResSucces)
+    }
+}
