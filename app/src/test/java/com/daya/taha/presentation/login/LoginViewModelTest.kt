f63d76ac242e613dbb9c9b894ce2ec47677a0b8b
ad <da>
2021-08-08 11:43:35 +0700

    add unit test dependency for app module

---
+++ app/src/test/java/com/daya/taha/presentation/login/LoginViewModelTest.kt
@@ -0,0 +1,18 @@
+package com.daya.taha.presentation.login
+
+
+import org.junit.Test
+
+
+class LoginViewModelTest {
+
+    lateinit var loginViewModel : LoginViewModel
+    @Test
+    fun getLoginStatus() {
+
+    }
+
+    @Test
+    fun login() {
+    }
+}
