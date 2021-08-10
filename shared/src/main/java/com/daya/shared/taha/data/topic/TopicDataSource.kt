6cad2964d75897147d1acecdc6e520db1fe6b629
ad <da>
2021-08-10 15:44:33 +0700

    add subcribe unsubscribe method to TopicDataSource

--- shared/src/main/java/com/daya/shared/taha/data/topic/TopicDataSource.kt
+++ shared/src/main/java/com/daya/shared/taha/data/topic/TopicDataSource.kt
@@ -3,12 +3,18 @@ package com.daya.shared.taha.data.topic
 import com.daya.shared.taha.domain.model.TopicNet
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.tasks.await
+import timber.log.Timber
 import javax.inject.Inject
+import kotlin.coroutines.resume
 
 
 interface TopicDataSource {
     suspend fun getDefaultTopic() :List<TopicNet>
+    suspend fun subscribeTopic(topic: TopicNet) : Boolean
+    suspend fun unSubscribeTopic(topic: TopicNet) : Boolean
+    suspend fun getSubScribedTopic() :List<String>
 }
 
 class FirebaseTopicDataSource
@@ -30,6 +36,38 @@ constructor(
             val isUnsubscribeAble = it.data?.get("isUnsubscribeAble") as Boolean
             TopicNet(topicId = topicId, topicName = name, isUnsubscribeAble = isUnsubscribeAble)
         }.toList()
+    }
+
+    override suspend fun subscribeTopic(topic: TopicNet) : Boolean = suspendCancellableCoroutine {continuation ->
+        messaging.subscribeToTopic(topic.topicName)
+            .addOnCompleteListener { task ->
+                val isSuccess = task.isSuccessful
+                val msg = if (isSuccess) {
+                    "subscribing to ${topic.topicName} success"
+                } else {
+                    "subscribing to ${topic.topicName} failed"
+                }
+                Timber.i(msg)
+                continuation.resume(isSuccess)
+            }
+    }
+
+    override suspend fun unSubscribeTopic(topic: TopicNet) : Boolean = suspendCancellableCoroutine { continuation ->
+        messaging.unsubscribeFromTopic(topic.topicName)
+            .addOnCompleteListener { task ->
+                val isSuccess = task.isSuccessful
+                val msg = if (task.isSuccessful) {
+                    "unsubscribed to ${topic.topicName} success"
+                } else {
+                    "unsubscribed to ${topic.topicName} failed"
+                }
+                Timber.i(msg)
+                continuation.resume(isSuccess)
+            }
+    }
+
+    override suspend fun getSubScribedTopic(): List<String> {
+        TODO("Not yet implemented")
     }
 
 }
