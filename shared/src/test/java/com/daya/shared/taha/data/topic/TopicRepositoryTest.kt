54191d343735f042b475242dd49291e9b7242ef4
ad <da>
2021-08-16 10:06:18 +0700

    intent detail notification

--- shared/src/test/java/com/daya/shared/taha/data/topic/TopicRepositoryTest.kt
+++ shared/src/test/java/com/daya/shared/taha/data/topic/TopicRepositoryTest.kt
@@ -4,10 +4,10 @@ package com.daya.shared.taha.data.topic
 import com.daya.shared.taha.testutil.Dummy
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 class TopicRepositoryTest {
@@ -29,5 +29,30 @@ class TopicRepositoryTest {
         val actual = topicRepository.getDefaultTopic()
 
         assertThat(actual).isEqualTo(dummyTopicNet)
+    }
+
+    @Test
+    fun subscribeToTopic() = runBlocking {
+        topicRepository.subscribeToTopic(dummyTopicNet[0])
+        verify(firebaseTopicDataSource).subscribeTopic(dummyTopicNet[0])
+    }
+
+    @Test
+    fun subscribeUserToDefaultTopic() = runBlocking {
+        topicRepository.subscribeUserToDefaultTopic()
+        verify(firebaseTopicDataSource).subscribingUserToDefaultTopic()
+    }
+
+    @Test
+    fun unsubscribeToTopic() = runBlocking {
+        topicRepository.unsubscribeToTopic(dummyTopicNet[0])
+        verify(firebaseTopicDataSource).unSubscribeTopic(dummyTopicNet[0])
+    }
+
+    @Test
+    fun getSubscribedTopic() = runBlocking {
+        whenever(firebaseTopicDataSource.getSubScribedTopic()).thenReturn(listOf("foo","bar"))
+        val actual = topicRepository.getSubscribedTopic()
+        assertThat(actual).isEqualTo(listOf("foo","bar"))
     }
 }
